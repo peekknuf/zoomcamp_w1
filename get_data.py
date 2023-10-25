d96faafd3c40f1e389c160f9ab9dab8ddf61c322
peekknuf <peekknuf@gmail.com>
2023-10-25 03:04:58 +0200

    downloading 20gb worth of data

---
+++ get_data.py
@@ -0,0 +1,32 @@
+import os
+import requests
+from datetime import datetime
+import time
+
+base_url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{}-{}.parquet"
+
+start_year = 2009
+end_year = 2023
+
+download_path = "/home/peekknuf/ny_taxi_data/files"
+
+for year in range(start_year, end_year + 1):
+    for month in range(1, 13): 
+        formatted_url = base_url.format(year, str(month).zfill(2))
+        filename = os.path.join(download_path, f"yellow_taxi_trips_{year}_{str(month).zfill(2)}.parquet")
+
+        if not os.path.exists(filename):
+            start_time = time.time()
+            dataset_response = requests.get(formatted_url)
+
+            if dataset_response.status_code == 200:
+                with open(filename, "wb") as dataset_file:
+                    dataset_file.write(dataset_response.content)
+                end_time = time.time()
+
+                duration = end_time - start_time
+                print(f"Downloaded: {filename} (Time: {duration:.2f} seconds)")
+            else:
+                print(f"Failed to download: {filename}")
+        else:
+            print(f"File already exists, skipping: {filename}")
