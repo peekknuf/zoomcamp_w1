2c1d24d2f00f1481b227ba971b917c8fd47390c7
peekknuf <peekknuf@gmail.com>
2024-03-24 18:51:56 +0100

    we're so back

--- pyspark_directory_ingestion.py
+++ pyspark_directory_ingestion.py
@@ -2,44 +2,54 @@ from pyspark.sql import SparkSession
 import logging
 import os
 
-def ingest_data_to_postgresql(user, password, host, port, db, schema_name, data_directory):
-    data_files = [f for f in os.listdir(data_directory) if f.endswith('.parquet')]
 
+def ingest_data_to_postgresql(
+    user, password, host, port, db, schema_name, data_directory
+):
     try:
-        spark = SparkSession.builder \
-            .appName("DataToPostgreSQL") \
-            .config("spark.driver.extraClassPath", "postgresql-42.6.0.jar") \
-            .config("spark.executor.extraClassPath", "postgresql-42.6.0.jar") \
+        spark = (
+            SparkSession.builder.appName("DataToPostgreSQL")
+            .config("spark.driver.extraClassPath", "postgresql-42.7.3.jar")
+            .config("spark.executor.extraClassPath", "postgresql-42.7.3.jar")
+            .config("spark.ui.port", "4041")
+            .config("spark.driver.host", "localhost")
             .getOrCreate()
+        )
 
-        for data_file in data_files:
-            table_name = data_file.split('.')[0]
-            data_df = spark.read.option("mergeSchema", "true").parquet(os.path.join(data_directory, data_file))
+        for data_file in os.listdir(data_directory):
+            if data_file.endswith(".parquet"):
+                table_name = os.path.splitext(data_file)[0]
+                data_df = spark.read.option("mergeSchema", "true").parquet(
+                    os.path.join(data_directory, data_file)
+                )
 
-            data_df.write \
-                .format("jdbc") \
-                .option("url", f"jdbc:postgresql://{host}:{port}/{db}") \
-                .option("dbtable", f"{schema_name}.{table_name}") \
-                .option("user", user) \
-                .option("password", password) \
-                .mode("overwrite") \
-                .save()
+                data_df.write.format("jdbc").option(
+                    "url", f"jdbc:postgresql://{host}:{port}/{db}"
+                ).option("dbtable", f"{schema_name}.{table_name}").option(
+                    "user", user
+                ).option("password", password).mode("overwrite").save()
 
-            logging.info(f"Data from {data_file} ingested to {schema_name}.{table_name} in PostgreSQL.")
+                logging.info(
+                    f"Data from {data_file} ingested to {schema_name}.{table_name} in PostgreSQL."
+                )
     except Exception as e:
         logging.error(f"Error ingesting data to PostgreSQL: {str(e)}")
 
+
 def main():
     user = "root"
-    password = "root"
+    password = "1234"
     host = "localhost"
     port = 5432
     db = "ny_taxi"
     schema_name = "public"
-    data_directory = "/home/peek/ny_taxi_data"
+    data_directory = "./ny_taxi_data/"
 
-    ingest_data_to_postgresql(user, password, host, port, db, schema_name, data_directory)
+    ingest_data_to_postgresql(
+        user, password, host, port, db, schema_name, data_directory
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     main()
